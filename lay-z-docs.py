2e73fe2ab886285f30a0c693aa242628d0a880ea
momopranto <m.amin@nyu.edu>
2016-02-15 00:21:46 -0500

    initial language support

--- lay-z-docs.py
+++ lay-z-docs.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python
 import json
 
-lines = None
-with open("test.cpp") as f:
-    lines = f.readlines()
-
-print lines
+# language: [definition style, comment style]
+P_LANG = {
+        'c':[DEF_STYLE[0],COM_STYLE[0]],
+        'c++':[DEF_STYLE[0],COM_STYLE[0]],
+        'c#':[DEF_STYLE[0],COM_STYLE[0]],
+        'python':[DEF_STYLE[1],COM_STYLE[1]],
+        'java':[DEF_STYLE[3],COM_STYLE[0]],
+        'ruby':[DEF_STYLE[1],COM_STYLE[1]],
+        'javascript':[DEF_STYLE[2],COM_STYLE[0]],
+        'php':[DEF_STYLE[2],COM_STYLE[0]+COM_STYLE[1]]
+        }
+DEF_STYLE = [
+        ['function identifier regex'],
+        ['def'],
+        ['function'],
+        ['java identifier regex']
+        ]
+COM_STYLE = [
+        ['//','/*','*/'],
+        ['#'],
+        ['--'],
+        ]
